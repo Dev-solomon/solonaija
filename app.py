3f110546df78b24aa17ee655428c873fa012e5df
Dev-solomon <ntiasolomon7@gmail.com>
2023-10-02 21:47:55 -0400

    updates for admin panel

--- app.py
+++ app.py
@@ -27,6 +27,54 @@ def view_template():
     return render_template('view.html')
 
 
+# ################################################
+#  THIS IS THE ADMIN PANEL
+# ###############################################
+@app.route('/admin/dashboard')
+def admin_home():
+    return render_template('admin/index.html')
+# users page
+@app.route('/admin/users')
+def admin_users():
+    return render_template('admin/users.html')
+# Sign a  new co-admin
+@app.route('/admin/co-admin')
+def co_admin():
+    return render_template('admin/signup.html')
+# Reviews Page
+@app.route('/admin/reviews')
+def user_reviews():
+    return render_template('admin/reviews.html')
+# manage a user page
+@app.route('/admin/manage_user')
+def manage_user():
+    return render_template('admin/edit-user.html')
+# add new item
+@app.route('/admin/item')
+def new_item():
+    return render_template('admin/add-item.html')
+# sign in co-admin
+@app.route('/admin/login')
+def login_coadmin():
+    return render_template('admin/signin.html')
+# Comments on users
+@app.route('/admin/comments')
+def comments():
+    return render_template('admin/comments.html')
+# wrong page
+@app.route('/admin/404')
+def invalid():
+    return render_template('admin/404.html')
+# Movie catalog page
+@app.route('/admin/catalog')
+def catalog():
+    return render_template('admin/catalog.html')
+# change password
+@app.route('/admin/forgot')
+def forgot():
+    return render_template('admin/forgot.html')
+
+
 
 
 
